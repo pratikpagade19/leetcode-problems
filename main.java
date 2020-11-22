13e60d26519e597202c215cad56ec4003d6e7d58
pratikpagade19 <pratikpagade2013@gmail.com>
2020-11-22 00:55:54 -0800

    modified main.java, added first.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 class Solution {
 
 	public static void main(String[] args){
-		
-		System.out.println("main code") ;
+		System.out.println("first line") ;
+		System.out.println("main add") ;
 	}
 }
